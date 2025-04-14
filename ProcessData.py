bc4fd8a59e81df201d7d2f15a271bd7bd57bead7
Scast13 <santiagocastillo@unomaha.edu>
2025-04-14 00:47:06 +0000

    done

--- ProcessData.py
+++ ProcessData.py
@@ -3,21 +3,59 @@
 #Date:
 #Assignment:
 
-import random
-
 def main():
-
-  #Open the files we will be using
-  inFile = open("names.dat", 'r')
-  outFile = open("StudentList.csv", 'w')
-
-  #Process each line of the input file and output to the CSV file
+    inFile = open("names.dat", 'r')
+    outFile = open("StudentList.csv", 'w')
 
 
+    for line in inFile:
+        data = line.split()
+        first = data[0]
+        last = data[1]
+        idNum = data[3]
+        major = data[4] [:3]
+        year_full = data [5]. lower()
 
-  #Close files in the end to save and ensure they are not damaged.
-  inFile.close()
-  outFile.close()
+        if year_full == "freshman" :
+            year = "FR"
+        elif year_full == "sophomore" :
+            year = "SO"
+        elif year_full == "junior" :
+            year = "JR"
+        elif year_full == "senior" :
+            year = "SR" 
+        else:
+            year = "NA"
+        
+        major_year = major + "-" + year
+    
+        
+
+        student_id = makeID(first, last , idNum)
+
+        output = last + "," + first + "," + student_id + major_year + "\n"
+        outFile.write(output)
+
+    inFile.close()
+    outFile.close()
+
+
+def makeID(first, last, idNum):
+    
+    idLen = len(idNum)
+
+    while len(last) < 5:
+        last = last + "x"
+
+    # Get last 3 digits of student ID (after removing dashes)
+    id =first[0] + last + idNum[idLen - 3: ]
+
+    # Build and return UserID
+    return id
+
+
+
+
 
 if __name__ == '__main__':
-  main()
+    main()
